1b6a89d4cd4de4ee033908615cb57cdcfbb9675e
teldufalsari <idunaev71@gmail.com>
2021-02-25 23:19:55 +0000

    Move constructor, refactoring.

--- tstring.cpp
+++ tstring.cpp
@@ -3,183 +3,184 @@
 
 namespace tld
 {
-	string::string() : text{}, size(0)
-	{}
-	
-	string::string(const string& that) : text{}, size(that.size)
-	{
-		for (size_t i = 0; i < max_strlen; i++)
-			this->text[i] = that.text[i];
+
+string::string() :
+	text_((char*) new char[Max_Str_Len]),
+	size_(0)
+{
+	text_[0] = '\0';
+}
+
+string::string(const string& that) :
+	text_((char*) new char[Max_Str_Len]),
+	size_(that.size_) 
+{
+	for (size_t i = 0; i < Max_Str_Len; i++)
+		this->text_[i] = that.text_[i];
+}
+
+string& string::operator =(const string& that)
+{
+	if (this != &that) {
+		for (size_t i = 0; i < Max_Str_Len; i++)
+			this->text_[i] = that.text_[i];
+		this->size_ = that.size_;
 	}
-	
-	string& string::operator =(const string& that)
-	{
-		if (this != &that)
-		{
-			for (size_t i = 0; i < max_strlen; i++)
-				this->text[i] = that.text[i];
-			
-			this->size = that.size;
-		}
-		
-		return *this;
+	return *this;
+}
+
+string operator +(const string& lht, const string& rht)
+{
+	string tmp(lht);
+	tmp.size_ += rht.size_;
+	size_t j = 0;
+	for (size_t i = lht.size_; i < tmp.size_; i++) {
+		tmp.text_[i] = rht.text_[j];
+		j++;
 	}
-	
-	string operator +(const string& lht, const string& rht)
-	{
-		string tmp(lht);
-		
-		tmp.size += rht.size;
-		
-		size_t j = 0;
-		for (size_t i = lht.size; i < tmp.size; i++)
-		{
-			tmp.text[i] = rht.text[j];
-			j++;
-		}
-		
-		return tmp;
-	}
-	
-	bool operator ==(const string& lht, const string& rht)
-	{
-		if (lht.size != rht.size)
+	return tmp;
+}
+
+bool operator ==(const string& lht, const string& rht)
+{
+	if (lht.size_ != rht.size_)
+		return false;
+	for (size_t i = 0; i < lht.size_; i++)
+		if (lht.text_[i] != rht.text_[i])
 			return false;
-		
-		for (size_t i = 0; i < lht.size; i++)
-			if (lht.text[i] != rht.text[i])
+	return true;
+}
+
+bool operator >(const string& lht, const string& rht)
+{
+	if (lht.size_ < rht.size_) {
+		for (size_t i = 0; i < lht.size_; i++)
+			if (lht.text_[i] > rht.text_[i])
+				return true;
+		return false;
+	} else {
+		for (size_t i = 0; i < rht.size_; i++)
+			if (lht.text_[i] < rht.text_[i])
 				return false;
-		
 		return true;
 	}
-	
-	bool operator >(const string& lht, const string& rht)
-	{
-		if (lht.size < rht.size)
-		{
-			for (size_t i = 0; i < lht.size; i++)
-				if (lht.text[i] > rht.text[i])
-					return true;
-			
-			return false;
-		} else
-		{
-			for (size_t i = 0; i < rht.size; i++)
-				if (lht.text[i] < rht.text[i])
-					return false;
-			
-			return true;
-		}
+}
+
+bool operator <(const string& lht, const string& rht)
+{
+	if (lht.size_ < rht.size_) {
+		for (size_t i = 0; i < lht.size_; i++)
+			if (lht.text_[i] > rht.text_[i])
+				return false;
+		return true;
+	} else {
+		for (size_t i = 0; i < rht.size_; i++)
+			if (lht.text_[i] < rht.text_[i])
+				return true;
+		return false;
 	}
-	
-	bool operator <(const string& lht, const string& rht)
+}
+
+bool operator !=(const string& lht, const string& rht)
+{
+	return !(lht == rht);
+}
+
+bool operator >=(const string& lht, const string& rht)
+{
+	return !(lht < rht);
+}
+
+bool operator <=(const string& lht, const string& rht)
+{
+	return !(lht > rht);
+}
+
+string& operator +=(string& lht, const string& rht)
+{
+	size_t j = 0;
+	while ((rht.text_[j] != '\0') && (lht.size_ < Max_Str_Len))
 	{
-		if (lht.size < rht.size)
-		{
-			for (size_t i = 0; i < lht.size; i++)
-				if (lht.text[i] > rht.text[i])
-					return false;
-			
-			return true;
-		} else
-		{
-			for (size_t i = 0; i < rht.size; i++)
-				if (lht.text[i] < rht.text[i])
-					return true;
-			
-			return false;
-		}
+		lht.text_[lht.size_] = rht.text_[j];
+		lht.size_++;
+		j++;
 	}
-	
-	bool operator !=(const string& lht, const string& rht)
+	lht.text_[lht.size_++] = '\0';	
+	return lht;
+}
+
+std::ostream& operator <<(std::ostream& os, const string& str)
+{
+	return __ostream_insert(os, str.text_, str.size_);
+}
+
+string::string(const char* str) :
+	text_((char*) new char[Max_Str_Len]),
+	size_(0)
+{
+	while ((str[size_] != '\0') && (size_ < Max_Str_Len))
 	{
-		return !(lht == rht);
+		text_[size_] = str[size_];
+		size_++;
 	}
-	
-	bool operator >=(const string& lht, const string& rht)
+	text_[Max_Str_Len] - '\0';
+}
+
+string& string::operator =(const char* str)
+{
+	this->size_ = 0;
+	while ((str[size_] != '\0') && (size_ < Max_Str_Len))
 	{
-		return !(lht < rht);
+		text_[size_] = str[size_];
+		size_++;
 	}
-	
-	bool operator <=(const string& lht, const string& rht)
+	text_[size_] = '\0';
+	return *this;
+}
+
+string& operator +=(string& lht, const char* str)
+{
+	size_t j = 0;
+	while ((str[j] != '\0') && (lht.size_ < Max_Str_Len))
 	{
-		return !(lht > rht);
+		lht.text_[lht.size_] = str[j];
+		lht.size_++;
+		j++;
 	}
-	
-	string& operator +=(string& lht, const string& rht)
+	lht.text_[lht.size_++] = '\0';
+	return lht;
+}
+
+string& operator +=(string& lht, const char rht)
+{
+	if (lht.size_ < Max_Str_Len)
 	{
-		size_t j = 0;
-		
-		while ((rht.text[j] != '\0') && (lht.size < max_strlen))
-		{
-			lht.text[lht.size] = rht.text[j];
-			lht.size++;
-			j++;
-		}
-		
-		lht.text[lht.size++] = '\0';
-		
-		return lht;
+		lht.text_[lht.size_++] = rht;
+		lht.text_[lht.size_] = '\0';
 	}
-	
-	std::ostream& operator <<(std::ostream& os, const string& str)
-	{
-		return __ostream_insert(os, str.text, str.size);
-	}
-	
-	string::string(const char* str) : text{0}, size(0)
-	{
-		while ((str[size] != '\0') && (size < max_strlen))
-		{
-			text[size] = str[size];
-			size++;
-		}
-	}
-	
-	string& string::operator =(const char* str)
-	{
-		this->size = 0;
-		
-		while ((str[size] != '\0') && (size < max_strlen))
-		{
-			text[size] = str[size];
-			size++;
-		}
-		
-		text[size] = '\0';
-		
-		return *this;
-	}
-	
-	string& operator +=(string& lht, const char* str)
-	{
-		
-		size_t j = 0;
-		
-		while ((str[j] != '\0') && (lht.size < max_strlen))
-		{
-			lht.text[lht.size] = str[j];
-			lht.size++;
-			j++;
-		}
-		
-		lht.text[lht.size++] = '\0';
-		
-		return lht;
-	}
-	
-	string& operator +=(string& lht, const char rht)
-	{
-		if (lht.size < max_strlen)
-		{
-			lht.text[lht.size++] = rht;
-			lht.text[lht.size] = '\0';
-		}
-		
-		return lht;
-	}
-	
-	
-	string::~string() = default;
-	
+	return lht;
+}
+
+
+string::~string()
+{
+	delete[] text_;	
+}
+
+string::string(string&& that) noexcept :
+	text_(that.text_),
+	size_(that.size_)
+{
+	that.text_ = nullptr;
+}
+
+
+
+string& string::operator =(string&& that) noexcept
+{
+	this->text_ = that.text_;
+	that.text_ = nullptr;
+	return *this;
+}
+
 } //namespace tld
--- tstring.h
+++ tstring.h
@@ -1,65 +1,63 @@
 #ifndef TLD_TSTRING_H
 #define TLD_TSTRING_H
 
-/*This string class may seem week now. But only now.
- * This is going to be a project showing development in progress.
- * This class shall grow and and gain its might to replace std::string one day.
- */
-
 #include <iostream>
+#include <string>
 
 
 namespace tld
 {
 
-	const size_t max_strlen = 1023;
+const size_t Max_Str_Len = 1023;
+
+class string
+{
+
 	
-	class string
+private:
+	char* text_;
+	size_t size_;
 	
-	{
+public:
+	string();
+
+	string(const string& that);
 	
-	private:
-		char text[max_strlen + 1];
-		size_t size;
+	string(string&& that) noexcept;
 	
-	private:
+	~string();
 	
-	public:
-		string();
-		
-		string(const string& that);
-		
-		~string();
-		
-		string(const char* str);
-		
-		string& operator =(const string& that);
-		
-		string& operator =(const char* str);
-		
-		friend string operator +(const string& lht, const string& rht);
-		
-		friend string& operator +=(string& lht, const string& rht);
-		
-		friend inline bool operator ==(const string& lht, const string& rht);
-		
-		friend inline bool operator >(const string& lht, const string& rht);
-		
-		friend inline bool operator <(const string& lht, const string& rht);
-		
-		friend inline bool operator !=(const string& lht, const string& rht);
-		
-		friend inline bool operator >=(const string& lht, const string& rht);
-		
-		friend inline bool operator <=(const string& lht, const string& rht);
-		
-		friend std::ostream& operator <<(std::ostream& os, const string& str);
-		
-		friend string& operator +=(string& lht, const char* rht);
-		
-		friend string& operator +=(string& lht, char rht);
-		
-	};
+	string(const char* str);
+	
+	string& operator =(const string& that);
+	
+	string& operator =(string&& that) noexcept;
+	
+	string& operator =(const char* str);
+	
+	friend string operator +(const string& lht, const string& rht);
+	
+	friend string& operator +=(string& lht, const string& rht);
+	
+	friend inline bool operator ==(const string& lht, const string& rht);
+	
+	friend inline bool operator >(const string& lht, const string& rht);
+
+	friend inline bool operator <(const string& lht, const string& rht);
+	
+	friend inline bool operator !=(const string& lht, const string& rht);
+	
+	friend inline bool operator >=(const string& lht, const string& rht);
+	
+	friend inline bool operator <=(const string& lht, const string& rht);
+	
+	friend std::ostream& operator <<(std::ostream& os, const string& str);
+	
+	friend string& operator +=(string& lht, const char* rht);
+	
+	friend string& operator +=(string& lht, char rht);
+	
+};
 
 
 } //namespace tld
